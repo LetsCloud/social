f9751f55cccfe944108cb592893b0bc746b20879
CR <csernikr@gmail.com>
2020-10-13 10:13:25 +0200

    init

---
+++ social-client/src/main/java/io/demo/social/client/SocialApplication.java
@@ -0,0 +1,39 @@
+package io.demo.social.client;
+
+import com.github.nalukit.nalu.client.application.IsApplication;
+import com.github.nalukit.nalu.client.application.annotation.Application;
+import com.github.nalukit.nalu.client.application.annotation.Debug;
+import com.github.nalukit.nalu.client.application.annotation.Filters;
+import com.github.nalukit.nalu.plugin.elemental2.client.DefaultElemental2Logger;
+import io.demo.social.client.filter.LoginFilter;
+
+/**
+ * Copyright (C) 2018 - 2019 Frank Hossfeld <frank.hossfeld@googlemail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+@Application(
+    context = SocialContext.class,
+    startRoute = "/login/login",
+    loader = SocialLoader.class
+)
+@Debug(
+    logger = DefaultElemental2Logger.class,
+    logLevel = Debug.LogLevel.DETAILED
+)
+@Filters(
+    filterClasses = LoginFilter.class
+)
+public interface SocialApplication extends IsApplication {
+	
+}
